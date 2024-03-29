9df98b864292be85d44e43f54c5db06924fa4afb
Stanley <stanley7342@gmail.com>
2024-03-29 23:08:28 +0800

    Add multithreaded tcp server for multiple connections

--- module/zigbee/ezmesh-zbgw/src/server.c
+++ module/zigbee/ezmesh-zbgw/src/server.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <pthread.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <arpa/inet.h>
 #include "Queue.h"
 #include "server.h"
 #include "common.h"
 
-
 #define SERV_PORT 10010
-
 #define TCP_BUFSIZ 1024
 
 int errno;
@@ -23,25 +24,103 @@ int nbytes;     		/* the number of read **/
 int tcp_send_flag ;
 int client_connect_flag=0 ;
 static int	disconnect_cnt ;
-char buf[TCP_BUFSIZ],sbuf[TCP_BUFSIZ];
-struct sockaddr_in myaddr; 	/* address of this service */
+char buf[TCP_BUFSIZ], sbuf[TCP_BUFSIZ];
+struct sockaddr_in myaddr; 	    /* address of this service */
 struct sockaddr_in client_addr; /* address of client    */
 char cst[255];
+
 //--------------------------------------------------------------------------------------------
 
 char Rx_Tcpbuffer[TCP_BUFSIZ];
 
+typedef struct tcp_server_thread
+{
+	bool isConnect;
+	int sock;
+	struct sockaddr_in client_addr;
+	struct tcp_server_thread *next;
+} tcp_server_thread_t;
+
+tcp_server_thread_t *clientList;
+
+void tcp_client_list_insert(tcp_server_thread_t **list, tcp_server_thread_t *p)
+{
+	// printf(LIGHT_RED"tcp_client_list_insert\r\n"NONE);
+
+	tcp_server_thread_t *q = *list;
+
+	if (q == 0)
+	{
+		*list = p;
+	}
+	else
+	{
+		while (q->next)
+		{
+			q = q->next;
+		}
+		q->next = p;
+	}
+}
+
+tcp_server_thread_t *tcp_client_list_delete(tcp_server_thread_t **list, int sock)
+{
+	// printf(LIGHT_RED"tcp_client_list_delete\r\n"NONE);
+
+	tcp_server_thread_t *p, *q;
+
+	if (*list == 0)
+	{
+		return 0;
+	}
+	p = *list;
+	if (p->sock == sock)
+	{
+		*list = p->next;
+		return p;
+	}
+	q = p->next;
+	while (q)
+	{
+		if (q->sock == sock)
+		{
+			p->next = q->next;
+			return q;
+		}
+		p = q;
+		q = q->next;
+	}
+	return 0;
+}
+
+tcp_server_thread_t *tcp_client_list_search(tcp_server_thread_t *list, int sock)
+{
+	// printf(LIGHT_RED"tcp_client_list_search\r\n"NONE);
+
+	tcp_server_thread_t *p = list;
+
+	while (p)
+	{
+		if (p->sock == sock)
+		{
+			return p;
+		}
+		p = p->next;
+	}
+	return 0;
+}
+
 void tcp_server_start(void)
 {
 	//	Get a socket into TCP/IP
-	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) <0) 
+	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
 	{
 		perror ("socket failed");
 		exit(1);
 	}
 
 	int val = 1;
-	setsockopt(socket_fd, SOL_SOCKET,SO_REUSEADDR,(void *)&val,sizeof(int));	
+	setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, (void *)&val, sizeof(int));	
 	
 	//	Set up our address
 	bzero ((char *)&myaddr, sizeof(myaddr));
@@ -66,74 +145,100 @@ void tcp_server_start(void)
 
 void tcp_server_send(uint8_t *s, uint16_t len)
 {	
-	if(tcp_send_flag==1 && client_connect_flag)
-	{
+	tcp_server_thread_t *p = clientList;
 
-		if (write(recfd, s, len) == -1) 
+	while (p)
+	{
+		if (write(p->sock, s, len) == -1) 
 		{
-			perror ("write to client error");
-			client_connect_flag = 0;
-			close(recfd);
+			perror("write to client error");
+			// client_connect_flag = 0;
+			p->isConnect = false;
+			close(p->sock);
 			//exit(1);
 		}
 		else
 		{		
-			printf("Send to client, Message:%p , len = %d\n",s, len);
+			// printf("Send to client, Message: %p, len = %d\n", s, len);
+			printf("Send to %s: %d, len: %d\r\n", 
+				inet_ntoa(p->client_addr.sin_addr), htons(p->client_addr.sin_port), len);
 		}
-		tcp_send_flag = 0;
+		p = p->next;
 	}
 }
 
+void *connection_handler(void *arg)
+{
+	char buffer[TCP_BUFSIZ];
+	size_t bytes_read;
+	tcp_server_thread_t *sock = arg;
+	
+	while (sock->isConnect)
+	{
+		memset(buffer, 0x0, TCP_BUFSIZ);
+		if ((bytes_read = read(sock->sock, buffer, sizeof(buffer))) < 0)
+		{
+			perror("read from client error");
+		}
+		else if (bytes_read > 0)
+		{
+			buffer[bytes_read] = 0;
+			printf("Received message from %s: %d\n",
+				inet_ntoa(client_addr.sin_addr), htons(client_addr.sin_port));
+			printf("Start save data to ring buffer....\n");
+			Queue_RX_Write(buffer, bytes_read);
+		}
+		else
+		{
+			// printf("Client connect timeout...\n");
+			sock->isConnect = false;
+			close(sock->sock);
+			// break;
+		}
+		usleep(1000000); //1ms
+	}
+	printf("Client disconnect from %s: %d\n",
+		inet_ntoa(client_addr.sin_addr), htons(client_addr.sin_port)); 
+	// printf("Wait fot next clinet connect...\n");	
+	sock->isConnect = false;
 
-void * Task_tcp_server_listen(void* arg)
+	tcp_server_thread_t *ptr;
+	ptr = tcp_client_list_delete(&clientList, sock->sock);
+	if (ptr != NULL)
+	{
+		free(ptr);
+	}
+}
+
+void *Task_tcp_server_listen(void* arg)
 {
 	static int	TaskTcpRxLenth_Flag;
 	static int	RxHopping=-1000;
 	int 	i;
 	
-
 	length = sizeof(client_addr);
 	printf("Server is ready to receive !!\n");
 	while (1)
 	{
-		if ((recfd = accept(socket_fd, (struct sockaddr *)&client_addr, &length)) <0) 
+		if ((recfd = accept(socket_fd, (struct sockaddr *)&client_addr, &length)) < 0)
 		{
-			perror ("could not accept call"); 
-	 	}
-		else
+			printf("connect error\r\n");
+		}
+		else 
 		{
-			printf("Client connect from %s : %d\n",inet_ntoa(client_addr.sin_addr), htons(client_addr.sin_port)); 
-			client_connect_flag = 1;
-			disconnect_cnt = 0;
-			while (client_connect_flag) 
-			{
-				memset(Rx_Tcpbuffer,0x00,TCP_BUFSIZ);
-				TaskTcpRxLenth_Flag = recv(recfd, Rx_Tcpbuffer, TCP_BUFSIZ,0);
+			printf("Client connect from %s: %d\n",
+				inet_ntoa(client_addr.sin_addr), htons(client_addr.sin_port)); 
+			
+			pthread_t t;
+			tcp_server_thread_t *pClient;
 
-				if ((TaskTcpRxLenth_Flag) < 0) 
-				{
-					perror("read of data error nbytes !");
-				}			
-				else if(TaskTcpRxLenth_Flag >0)
-				{
-					Rx_Tcpbuffer[TaskTcpRxLenth_Flag] = 0x00;										
-					printf("Received message from %s : %d\n",inet_ntoa(client_addr.sin_addr), htons(client_addr.sin_port));
-					printf("Start save data to ring buffer....\n");
-					Queue_RX_Write(Rx_Tcpbuffer,TaskTcpRxLenth_Flag);
-					
-					disconnect_cnt = 0;
-				}
-				else
-				{
-						printf("Client connect timeout...\n");
-						client_connect_flag = 0;
-						close(recfd);
-						break;
-				}
-			}
-			printf("Client disconnect from %s : %d\n",inet_ntoa(client_addr.sin_addr), htons(client_addr.sin_port)); 
-			printf("Wait fot next clinet connect...\n");	
-			client_connect_flag = 0;
+			pClient = malloc(sizeof(tcp_server_thread_t));
+			pClient->next = NULL;
+			pClient->isConnect = true;
+			pClient->sock = recfd;
+			pClient->client_addr = client_addr;
+			tcp_client_list_insert(&clientList, pClient);
+			pthread_create(&t, NULL, connection_handler, pClient);
 		}
 		usleep(1000000); //1ms
 	}
